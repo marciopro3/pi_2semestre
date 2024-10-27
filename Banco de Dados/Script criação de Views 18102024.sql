1d224a65b25bc901759808c6337cdbf39a8a9987
MÁRCIO SOARES <marciopro3@gmail.com>
2024-10-27 19:33:40 -0300

    View do BI Atualizada

--- Banco de Dados/Script criação de Views 18102024.sql
+++ Banco de Dados/Script criação de Views 18102024.sql
@@ -1,59 +1,49 @@
-## VIEW DE UNINDO TODAS AS INFORMAÇÕES QUE IMPORTAM PARA O POWER BI
-
-CREATE VIEW `SBOReciclaSV`.`view_completa` AS
-SELECT DISTINCT
-    u.nome AS usuario_nome,
-    u.email AS usuario_email,
-    u.telefone AS usuario_telefone,
-    u.dataCadastro AS usuario_dataCadastro,
-    tu.tipo AS tipo_usuario,
-    
-    c.nome AS cidade_nome,
-    
-    d.nome AS deposito_nome,
-    d.endereco AS deposito_endereco,
-    d.cep AS deposito_cep,
-    d.telefone AS deposito_telefone,
-    d.capacidadeMaxima AS deposito_capacidadeMaxima,
-    
-    mat.nome AS material_nome,
-    mat.descricao AS material_descricao,
-    
-    en.dataEntrada AS entrada_data,  -- Adicionando a data da entrada
-     
-    sa.dataSaida AS saida_data,
-    sa.quantidade AS quantidade_saida,
-    
-    col.nome AS coletor_nome,
-    col.cnpjCpf AS coletor_documento,
-    col.telefone AS coletor_telefone,
-    
-    c2.nome AS coletor_cidade_nome,
-    
-    a.data AS agendamento_data,
-    a.hora AS agendamento_hora,
-    a.status AS agendamento_status
-
-FROM 
-    `SBOReciclaSV`.`usuario` u
-INNER JOIN 
-    `SBOReciclaSV`.`tipoUsuario` tu ON u.tipoUsuario_idtipoUsuario = tu.idtipoUsuario
-
-INNER JOIN 
-    `SBOReciclaSV`.`entrada` en ON u.idusuario = en.usuario_idusuario
-INNER JOIN 
-    `SBOReciclaSV`.`deposito` d ON en.deposito_iddeposito = d.iddeposito
-INNER JOIN 
-    `SBOReciclaSV`.`cidade` c ON d.cidade_idcidade = c.idcidade
-INNER JOIN 
-    `SBOReciclaSV`.`estado` e ON c.estado_idestado = e.idestado
-INNER JOIN 
-    `SBOReciclaSV`.`material` mat ON en.material_idmaterial = mat.idmaterial
-INNER JOIN 
-    `SBOReciclaSV`.`saida` sa ON d.iddeposito = sa.deposito_iddeposito
-INNER JOIN 
-    `SBOReciclaSV`.`coletor` col ON sa.coletor_idcoletor = col.idcoletor
-INNER JOIN 
-    `SBOReciclaSV`.`cidade` c2 ON col.cidade_idcidade = c2.idcidade
-LEFT JOIN 
-    `SBOReciclaSV`.`agendamento` a ON col.idcoletor = a.coletor_idcoletor;
+CREATE VIEW SBOReciclaSV.vw_completa AS
+SELECT
+    u.idusuario AS UsuarioID,
+    u.nome AS UsuarioNome,
+    u.email AS UsuarioEmail,
+    u.telefone AS UsuarioTelefone,
+    u.dataCadastro AS UsuarioDataCadastro,
+    tu.tipo AS TipoUsuario,
+    c.nome AS ColetorNome,
+    c.cnpjCpf AS ColetorCnpjCpf,
+    c.telefone AS ColetorTelefone,
+    c.endereco AS ColetorEndereco,
+    cid_c.nome AS ColetorCidade,
+    est_c.nome AS ColetorEstado,
+    a.idagendamento AS AgendamentoID,
+    a.data AS AgendamentoData,
+    a.hora AS AgendamentoHora,
+    a.status AS AgendamentoStatus,
+    m.nome AS MaterialNome,
+    m.descricao AS MaterialDescricao,
+    cat.categoria AS CategoriaMaterial,
+    dep.nome AS DepositoNome,
+    dep.endereco AS DepositoEndereco,
+    dep.cep AS DepositoCEP,
+    cid_d.nome AS DepositoCidade,
+    est_d.nome AS DepositoEstado,
+    dep.telefone AS DepositoTelefone,
+    dep.capacidadeMaxima AS DepositoCapacidadeMaxima,
+    dep.dataCadastro AS DepositoDataCadastro,
+    e.identrada AS EntradaID,
+    e.dataEntrada AS EntradaData,
+    e.quantidade AS EntradaQuantidade,
+    s.idsaida AS SaidaID,
+    s.dataSaida AS SaidaData,
+    s.quantidade AS SaidaQuantidade
+FROM
+    SBOReciclaSV.usuario u
+    INNER JOIN SBOReciclaSV.tipoUsuario tu ON u.tipoUsuario_idtipoUsuario = tu.idtipoUsuario
+    LEFT JOIN SBOReciclaSV.entrada e ON u.idusuario = e.usuario_idusuario
+    LEFT JOIN SBOReciclaSV.material m ON e.material_idmaterial = m.idmaterial
+    LEFT JOIN SBOReciclaSV.categoria cat ON m.categoria_idcategoria = cat.idcategoria
+    LEFT JOIN SBOReciclaSV.deposito dep ON e.deposito_iddeposito = dep.iddeposito
+    LEFT JOIN SBOReciclaSV.cidade cid_d ON dep.cidade_idcidade = cid_d.idcidade
+    LEFT JOIN SBOReciclaSV.estado est_d ON cid_d.estado_idestado = est_d.idestado
+    LEFT JOIN SBOReciclaSV.saida s ON s.coletor_idcoletor = e.usuario_idusuario
+    LEFT JOIN SBOReciclaSV.coletor c ON s.coletor_idcoletor = c.idcoletor
+    LEFT JOIN SBOReciclaSV.cidade cid_c ON c.cidade_idcidade = cid_c.idcidade
+    LEFT JOIN SBOReciclaSV.estado est_c ON cid_c.estado_idestado = est_c.idestado
+    LEFT JOIN SBOReciclaSV.agendamento a ON a.coletor_idcoletor = c.idcoletor;
